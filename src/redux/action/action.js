80db8d73e3f8f3502fe182bbc9a2fcc0db387100
Mir Ahmed Raza <98475059+abusultan77@users.noreply.github.com>
2024-01-04 11:44:07 +0500

    raza

--- src/redux/action/action.js
+++
@@ -1,7 +0,0 @@
-export const inputAction=(data)=>{
-     return{
-       type:'INPUTADDRESS',
-       payload:data
-     }
-      
-    }
--- src/redux/store/rootReduce.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { Auth } from "../reducer/reducer";
-
-const rootReducer = combineReducers({
-  Auth:Auth
-});
-
-export default rootReducer;
