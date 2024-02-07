dd128599a2bd8321a70dd4edae8ecb2b7607b9b6
abusultan77 <mirahmedrazajee@gmail.com>
2024-02-07 12:25:13 +0500

    raza

--- src/views/Applications/Applications.js
+++ src/views/Applications/Applications.js
@@ -134,56 +134,59 @@ useEffect(() => {
                                     <Dropdown.Item href="#/action-2">Following</Dropdown.Item>
                                 </Dropdown.Menu>
                             </Dropdown> */}
-                            <Dropdown className="filyerbyns ">
-                                <Dropdown.Toggle className="filyerbynss" id="dropdown-basic">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" viewBox="0 0 21 20" fill="none">
-                                        <path d="M9.49329 18.875C9.09329 18.875 8.70163 18.775 8.33496 18.575C7.60163 18.1667 7.15996 17.425 7.15996 16.5917V12.175C7.15996 11.7583 6.88496 11.1333 6.62663 10.8167L3.50996 7.51666C2.98496 6.99167 2.58496 6.09167 2.58496 5.41667V3.5C2.58496 2.16667 3.59329 1.125 4.87663 1.125H15.8766C17.1433 1.125 18.1683 2.15 18.1683 3.41667V5.25C18.1683 6.125 17.6433 7.11666 17.1516 7.60833L13.5433 10.8C13.1933 11.0917 12.9183 11.7333 12.9183 12.25V15.8333C12.9183 16.575 12.4516 17.4333 11.8683 17.7833L10.7183 18.525C10.3433 18.7583 9.91829 18.875 9.49329 18.875ZM4.87663 2.375C4.29329 2.375 3.83496 2.86667 3.83496 3.5V5.41667C3.83496 5.725 4.08496 6.325 4.40163 6.64166L7.57663 9.98333C8.00163 10.5083 8.41829 11.3833 8.41829 12.1667V16.5833C8.41829 17.125 8.79329 17.3917 8.95163 17.475C9.30163 17.6667 9.72663 17.6667 10.0516 17.4667L11.21 16.725C11.4433 16.5833 11.6766 16.1333 11.6766 15.8333V12.25C11.6766 11.3583 12.11 10.375 12.735 9.85L16.3016 6.69166C16.585 6.40833 16.9266 5.73333 16.9266 5.24167V3.41667C16.9266 2.84167 16.46 2.375 15.885 2.375H4.87663Z" fill="#862FC0" />
-                                        <path d="M5.37652 8.95839C5.25985 8.95839 5.15152 8.92506 5.04319 8.86672C4.75152 8.68339 4.65986 8.29172 4.84319 8.00006L8.95152 1.41672C9.13485 1.12506 9.51819 1.03339 9.80985 1.21672C10.1015 1.40006 10.1932 1.78339 10.0099 2.07506L5.90152 8.65839C5.78486 8.85006 5.58485 8.95839 5.37652 8.95839Z" fill="#862FC0" />
-                                    </svg>
-                                    Filters
-                                </Dropdown.Toggle>
-                                <Dropdown.Menu>
-                                <div className="inneritem">
-                                    All
-                                    <div className="main-outer-p">
+                          <Dropdown className="filyerbyns ">
+              <Dropdown.Toggle className="filyerbynss" id="dropdown-basic">
+                <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" viewBox="0 0 21 20" fill="none">
+                  <path d="M9.49329 18.875C9.09329 18.875 8.70163 18.775 8.33496 18.575C7.60163 18.1667 7.15996 17.425 7.15996 16.5917V12.175C7.15996 11.7583 6.88496 11.1333 6.62663 10.8167L3.50996 7.51666C2.98496 6.99167 2.58496 6.09167 2.58496 5.41667V3.5C2.58496 2.16667 3.59329 1.125 4.87663 1.125H15.8766C17.1433 1.125 18.1683 2.15 18.1683 3.41667V5.25C18.1683 6.125 17.6433 7.11666 17.1516 7.60833L13.5433 10.8C13.1933 11.0917 12.9183 11.7333 12.9183 12.25V15.8333C12.9183 16.575 12.4516 17.4333 11.8683 17.7833L10.7183 18.525C10.3433 18.7583 9.91829 18.875 9.49329 18.875ZM4.87663 2.375C4.29329 2.375 3.83496 2.86667 3.83496 3.5V5.41667C3.83496 5.725 4.08496 6.325 4.40163 6.64166L7.57663 9.98333C8.00163 10.5083 8.41829 11.3833 8.41829 12.1667V16.5833C8.41829 17.125 8.79329 17.3917 8.95163 17.475C9.30163 17.6667 9.72663 17.6667 10.0516 17.4667L11.21 16.725C11.4433 16.5833 11.6766 16.1333 11.6766 15.8333V12.25C11.6766 11.3583 12.11 10.375 12.735 9.85L16.3016 6.69166C16.585 6.40833 16.9266 5.73333 16.9266 5.24167V3.41667C16.9266 2.84167 16.46 2.375 15.885 2.375H4.87663Z" fill="#862FC0" />
+                  <path d="M5.37652 8.95839C5.25985 8.95839 5.15152 8.92506 5.04319 8.86672C4.75152 8.68339 4.65986 8.29172 4.84319 8.00006L8.95152 1.41672C9.13485 1.12506 9.51819 1.03339 9.80985 1.21672C10.1015 1.40006 10.1932 1.78339 10.0099 2.07506L5.90152 8.65839C5.78486 8.85006 5.58485 8.95839 5.37652 8.95839Z" fill="#862FC0" />
+                </svg>
+                Filters
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <div className="inneritem">
+                  All
+                  {/* <div className="main-outer-p">
 
                                         <div className="main-switch-nns">
                                             <div class="custom-control custom-switchs">
-                                                <input checked={all}
-                                                    onChange={(e) => handleRemoveFilter(e)} type="checkbox" class="custom-control-input" id="customSwitches1" />
-                                                <label class="custom-control-label" for="customSwitches1"></label>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="inneritem">
-                                    Open Edition
-                                    <div className="main-outer-p">
-
-                                        <div className="main-switch-nns">
-                                            <div class="custom-control custom-switchs">
-                                                <input checked={verify}
-                                                    onChange={(e) => handleVerifyFilter(e)} type="checkbox" class="custom-control-input" id="customSwitches2" />
+                                                <input type="checkbox" class="custom-control-input" id="customSwitches2" />
                                                 <label class="custom-control-label" for="customSwitches2"></label>
                                             </div>
                                         </div>
-                                    </div>
-                                </div>
-                                <div className="inneritem">
-                                    limited Edition
-                                    <div className="main-outer-p">
+                                    </div> */}
+                  <div class="content">
+                    <label class="checkBox">
+                      <input id="ch1" type="checkbox" />
+                      <div class="transition"></div>
+                    </label>
+                  </div>
+                </div>
+                <div className="inneritem">
+                  Limited Edition
 
-                                        <div className="main-switch-nns">
-                                            <div class="custom-control custom-switchs">
-                                                <input checked={block}
-                                                    onChange={(e) => handleBlockFilter(e)} type="checkbox" class="custom-control-input" id="customSwitches3" />
-                                                <label class="custom-control-label" for="customSwitches3"></label>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                </Dropdown.Menu>
-                            </Dropdown>
+
+                  <div class="content">
+                    <label class="checkBox">
+                      <input id="ch1" type="checkbox" />
+                      <div class="transition"></div>
+                    </label>
+                  </div>
+
+                </div>
+                <div className="inneritem">
+                  Open Edition
+
+
+                  <div class="content">
+                    <label class="checkBox">
+                      <input id="ch1" type="checkbox" />
+                      <div class="transition"></div>
+                    </label>
+
+                  </div>
+                </div>
+              </Dropdown.Menu>
+            </Dropdown>
                         </div>
 
 
