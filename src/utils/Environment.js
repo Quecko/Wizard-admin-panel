c72fff41a96f3d9d47efb6d0e78c268f368b921e
Abdullah-39 <113667444+Abdullah-39@users.noreply.github.com>
2024-02-15 22:53:11 +0500

    mm

--- src/utils/Environment.js
+++ src/utils/Environment.js
@@ -1,6 +1,6 @@
 let Environment = {
-  api_url: "http://192.168.20.67:8080",
-//  api_url: "https://wizardapi.quecko.com",
+  // api_url: "http://192.168.20.67:8080",
+ api_url: "https://wizardapi.quecko.com",
 };
 
 export default Environment;
