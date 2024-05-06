b8a559403577e37a8c14e5434632c486e33697d7
1020amoeez <113603154+1020amoeez@users.noreply.github.com>
2024-05-06 17:13:42 +0500

    staging plateform fee

--- src/utils/Environment.js
+++ src/utils/Environment.js
@@ -1,9 +1,9 @@
 let Environment = {
   // api_url: "http://192.168.20.69:8080",
-//  api_url: "https://api.wizardgallery.xyz",
+  //  api_url: "https://api.wizardgallery.xyz",
   api_url: "https://wizardapi.quecko.com",
-//  api_url: "http://ec2-18-236-118-29.us-west-2.compute.amazonaws.com:8088",
-//  api_url: "http://ec2-54-191-120-95.us-west-2.compute.amazonaws.com:8080",
+  //  api_url: "http://ec2-18-236-118-29.us-west-2.compute.amazonaws.com:8088",
+  //  api_url: "http://ec2-54-191-120-95.us-west-2.compute.amazonaws.com:8080",
 
 
 };
