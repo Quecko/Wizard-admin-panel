b8102e3d4fa735b02d9dec5c61ece06d0bfcf91d
1020amoeez <113603154+1020amoeez@users.noreply.github.com>
2024-08-17 03:14:49 +0500

    launchpad contract changed

--- src/utils/ContractEnvironment.js
+++ src/utils/ContractEnvironment.js
@@ -1,7 +1,7 @@
 let Environment = {
   // CreaterAddress: "0x6ab3beac4f5f0fE1c6f0CE38d20964225Be68A31",
   // for mainnet
-  LaunchpadContract: "0x6B3188AC879b3ba14D79e76068f22E2E99598CF0",
+  LaunchpadContract: "0x3159dD9B5Bb98929BCd12f3678D8F73f48A037Eb",
   // MarketAddress: "0x9c5F504457C3d7D073AfaD3248F96681c890F43b",
 
   // for testnet
